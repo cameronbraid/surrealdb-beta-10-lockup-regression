a3118cc9ded79bd5f8257877c0e4d76e442e4120
Cameron Braid <cameron@drivenow.com.au>
2023-09-05 13:06:53 +1000

    beta9

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
-use std::collections::BTreeMap;
+use std::{collections::BTreeMap, str::FromStr};
 
-use rust_decimal_macros::dec;
-use surrealdb::sql::*;
+use bigdecimal::BigDecimal;
+use surrealdb::{sql::*, opt::auth::Root};
 
 #[tokio::main]
 async fn main() {
-    let surreal = surrealdb::engine::any::connect(format!("ws://localhost:22773"))
+    let surreal = surrealdb::engine::any::connect(format!("ws://localhost:22774"))
         .await
         .unwrap();
 
+
+    surreal.signin(Root {
+        username: "root",
+        password: "root",
+    }).await.unwrap();
+
     surreal.use_ns("test").use_db("test").await.unwrap();
 
-    let fields = BTreeMap::from([("f".to_owned(), Value::from(dec![0.1]))]); // this worked in beta 9
-    
-    // let fields = BTreeMap::from([("f".to_owned(), dec![0.1])]); // this works but ends up with `f` as a string
+
+    let fields = BTreeMap::from([(
+        "f".to_owned(),
+        Value::from(BigDecimal::from_str("0.1").unwrap()),
+    )]);
 
     surreal
         .query("UPDATE type::thing($table, $id) set f = $fields.f")
@@ -34,22 +42,4 @@ async fn main() {
 
     println!("{res:?}",);
 
-    surreal
-        .query("UPDATE type::thing($table, $id) content $fields")
-        .bind(("table", "a"))
-        .bind(("id", 1))
-        .bind(("fields", &fields))
-        .await
-        .unwrap()
-        .check()
-        .unwrap();
-
-    let res = surreal
-        .query("select * from a:1")
-        .await
-        .unwrap()
-        .check()
-        .unwrap();
-
-    println!("{res:?}",);
 }
